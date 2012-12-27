ed06c6ebb9bee49773c76d20140d72f70d34391d
Averrin <averrin@gmail.com>
2012-12-27 15:17:11 +0400

    mongo connection

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,20 @@
 /*
  * GET home page.
  */
+var mongo = require('mongo-lite');
+var util = require('util');
+var _ = require('underscore');
+
+db = mongo.connect(util.format('mongodb://%s:%s@averr.in:27017/%s', 'averrin', 'aqwersdf', 'orlangur'))
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    db.collectionNames(
+        function(err, docs){
+            if(err){
+                console.log(err);
+                return false;
+            }
+            res.render('index', { collection: docs });
+        }
+    )
 };
